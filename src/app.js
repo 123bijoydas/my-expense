ce898e700ac807df047348b3211e139282bdf070
bijoy <bijoydas351491@gmail.com>
2022-07-02 23:36:40 +0530

    some changes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
+//https://my-expense.herokuapp.com/expenses  ---------->API
+
 const express=require("express");
 require("./db/conn");
 const Expenses=require("./models/expense");
 const app=express();
+const cors=require("cors");
 const port=process.env.PORT||8000;
+app.use(cors());
 app.use(express.json());
+
 // app.get("/",(req,res)=>{
 //     res.send("hello from the server by bijoy");
 // })
@@ -32,8 +37,25 @@ app.get("/expenses",async(req,res)=>{
      }
 })
 
+app.delete("/expenses/:id",async(req,res)=>{
+    console.log(req.body);
+    try{
+        const deleteExpenses=await Expenses.findByIdAndDelete(req.params.id);
+        if(!req.params.id)
+        {
+            return res.status(404).send();
+        }
+        res.send(deleteExpenses);
+    }catch(e){
+        res.status(400).send(e.message);
+    }
+})
+
 app.listen(port,()=>{
     console.log(`connection is setup ${port}`)
 })
 
+// https://my-expense.herokuapp.com/expenses
+
 // https://my-expense.herokuapp.com/ | https://git.heroku.com/my-expense.git
+
